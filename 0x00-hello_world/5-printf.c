f0b143fd44334e2d94dea40c7732b3d4af5105b1
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-13 09:39:50 -0700

    done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point for my program
+ *
+ * Description: This it funtion that runs everything
+ * Return: This function return the value 0 to indicate success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
