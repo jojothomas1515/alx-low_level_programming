583deae60218285967b89bb05c7ee9a202a042ae
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-13 09:20:32 -0700

    push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - the entry point for my program
+ *
+ * Description: This is a longer description
+ * Return: return the value 0  for success code
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
