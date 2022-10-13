afc1a4f156f83985190f31dde80f8a0d81f9984f
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-13 10:39:09 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,6 @@ int main(void)
 	printf("Size of a int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a double: %d byte(s)\n", sizeof(double));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
