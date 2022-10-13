8e21638381c8a92ab2e02bb905fc180fdf2669fa
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-13 12:30:42 -0700

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - entry point of the c program
+ *
+ * Description: yes
+ * Return: done
+ */
+
+int main()
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015- \
+10-19");
+	return (1);
+}
