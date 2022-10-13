8cc5ba56b190dd449e3cac381ab719c2be41737e
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-13 10:30:56 -0700

    done

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of my program
+ *
+ * Description: this program print or the space each datatype occupy on machine
+ * architechture
+ * Return: Returns the success code 0;
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a double: %d byte(s)\n", sizeof(double));
+}
