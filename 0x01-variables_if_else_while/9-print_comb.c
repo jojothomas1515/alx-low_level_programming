30093fdbda68fa8170c639d5a1b9d7933c201aef
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-14 16:00:06 -0700

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Program Entry Point
+ *
+ * Description: Long desc
+ * Return: Returns 0 as success code
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		putchar(',');
+		putchar(' ');
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
