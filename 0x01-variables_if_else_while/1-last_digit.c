58bc7b948ce0752afe8b305eadf398d266c407cf
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-14 04:33:09 -0700

    done

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,8 +9,9 @@
 /**
  * main - Entry point for my program
  *
- * Description: this is my long description
- * Return: returns 0
+ * Description: This program takes the first and last number in in my number
+ * and check which is greater
+ * Return: Returns 0, the success exit code
  */
 
 int main(void)
@@ -20,17 +21,5 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else
-	{
-		printf("%d is zero\n", n);
-	}
 	return (0);
 }
