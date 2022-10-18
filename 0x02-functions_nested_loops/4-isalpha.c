e34d27d702ade3f1648ba48ff704b1b0694f90dd
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-18 14:37:38 -0700

    done

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - check of character is lower
+ * @c: character argument to be checked
+ * description: long description
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && <= 90 || c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c~
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - check of character is lower
+ * @c: character argument to be checked
+ * description: long description
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	if (c < 97 || c > 122)
+	{
+		return (0);
+	}
+	return (1);
+}
