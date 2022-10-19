d705e8bc41d8805a67d31f76847170eadb628bac
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-19 08:02:42 -0700

    done

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of any number
+ * @num: the digit
+ *
+ * Description: prints last digit
+ * Return: The last digit
+ */
+
+int print_last_digit(int num)
+{
+	return (num % 10);
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int main(void)
+{
+	int result = _abs(-6);
+	_putchar(65);
+	_putchar('\n');
+	return (0);
+}
