f0dac6f1344d167878d919589ec5872a6f9f5358
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-19 15:05:29 -0700

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,6 +17,7 @@ void print_to_98(int n)
 			if (n == 98)
                         {
                                 printf("%d\n", n);
+				n--;
                                 continue;
                         }
 
@@ -31,6 +32,7 @@ void print_to_98(int n)
 			if (n == 98)
 			{
 				printf("%d\n", n);
+				n--;
 				continue;
 			}
 			printf("%d, ", n);
