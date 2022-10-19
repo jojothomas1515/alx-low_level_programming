f148f39caaa29b0150f44e9b30eda42b5b2d0cc1
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-19 10:33:17 -0700

    done

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - times table
+ *
+ * Description: the multiplication table
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int m = 0, n;
+
+	while (m < 10)
+	{
+		n = 0;
+		while (n < 10)
+		{
+			int num = m * n;
+			int fd = num / 10;
+			int ld = num / 10;
+
+			_putchar(fd + 48);
+			if (ld != 0)
+			{
+				_purchar(ld + 48);
+			}
+
+			if (n != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			_putchar('\n');
+			n++;
+		}
+		m++;
+	}
+}
