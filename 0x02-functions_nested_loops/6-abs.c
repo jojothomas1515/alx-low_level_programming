20a78fb1797ac1353234dce9139f621c55cb4b91
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-19 07:21:52 -0700

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs : print the absolute value
+ *
+ * Description: this is a long description
+ * Return: it void so no return value
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return (num*-1);
+	}
+	return (num);
+}
