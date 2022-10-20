331dcd932046cd7a1e30bcc927182cfae0cfa28d
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-20 09:03:15 -0700

    done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - multiply any two num
+ * @a: first param
+ * @b: second param
+ *
+ * Description: long description
+ * Return: the product of @a and @b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
