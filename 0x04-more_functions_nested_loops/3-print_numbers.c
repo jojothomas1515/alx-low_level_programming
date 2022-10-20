1fb0be19a7b2996d8d7445407b3f2567fc70f4b0
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-20 09:43:14 -0700

    done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - this all base ten numbers
+ *
+ * Description: long decs
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i; i < 10; i++)
+	{
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
