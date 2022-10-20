145f645b11fce1b5f24f9ba927dc5207b523e913
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-20 11:56:32 -0700

    done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diag
+ * @n: num of iteration
+ *
+ * Description: done
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 1; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+			_purchar('\\');
+			_putchar('$');
+			_putchar('\n');
+		}
+	}
+}
