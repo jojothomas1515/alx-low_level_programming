6d0d018384d3d346bfd26b03d5422b2448c5259a
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-20 10:49:28 -0700

    done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - print line
+ *
+ * Description: done
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('\n');
+	}
+	_purchar('$');
+	_putchar('\n');
+}
