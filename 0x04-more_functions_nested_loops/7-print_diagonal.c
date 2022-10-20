1240f5158593e1de783de06bcb857f5fc9bac494
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-20 12:06:36 -0700

    done

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,11 +18,11 @@ void print_diagonal(int n)
 	{
 		for (int i = 1; i <= n; i++)
 		{
-			for (int j = 1; j <= i; j++)
+			for (int j = 1; j <= n; j++)
 			{
 				_putchar(' ');
 			}
-			_purchar('\\');
+			_putchar('\\');
 			_putchar('$');
 			_putchar('\n');
 		}
