f25313963803ffcf2bbeeff331133cdce43a56cd
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-20 17:00:03 -0700

    done

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - print square
+ * @size: size of the square
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
