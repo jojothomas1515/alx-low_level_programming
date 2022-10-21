97f60df4c241e63706d7ada9f95c07d913c24c42
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-22 00:51:31 +0100

    why

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,14 +10,14 @@
 
 void print_diagonal(int n)
 {
+    int i, j;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-        int i,j;
-
 		for (i = 1; i <= n; i++)
 		{
 			for (j = 1; j <= i; j++)
