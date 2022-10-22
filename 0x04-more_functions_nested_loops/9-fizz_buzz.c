36378435201b054f622b6c9cf6f7a46fa62d570e
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-21 17:26:50 -0700

    done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,23 +7,25 @@
 
 int main(void)
 {
-    int i;
-	for (i = 1; i <= 100; i++) {
-        if ((i % 3) == 0 && (i % 5) == 0)
-        {
-            printf("FizzBuZZ");
-        }
-        else if ((i % 3) == 0) {
-            printf("Fizz");
-        } else if ((i % 5) == 0) {
-            printf("Buzz");
-        }
-        else
-        {
-            printf("%d");
-        }
-        if (i != 100)
-            putchar(' ');
-    }
-    printf("\n");
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuZZ");
+		}
+		else if ((i % 3) == 0) {
+			printf("Fizz");
+		} else if ((i % 5) == 0) {
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d");
+		}
+		if (i != 100)
+			putchar(' ');
+	}
+	printf("\n");
 }
