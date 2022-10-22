f66dd7c6cf71d009b68230e78894e5a191aae7dc
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-21 18:00:41 -0700

    done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,9 +15,12 @@ int main(void)
 		{
 			printf("FizzBuzz");
 		}
-		else if ((i % 3) == 0) {
+		else if ((i % 3) == 0)
+		{
 			printf("Fizz");
-		} else if ((i % 5) == 0) {
+		}
+		else if ((i % 5) == 0)
+		{
 			printf("Buzz");
 		}
 		else
