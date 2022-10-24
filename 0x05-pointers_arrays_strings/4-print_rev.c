bc95f2ccbe4e96c6cda1a1cb92689d95d8ebd8b9
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-25 00:16:29 +0100

    done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,6 @@
-#include <unistd.h>
 #include <string.h>
+#include <stdio.h>
+
 /**
  * print_rev - print the rev of the string
  * @s: the string variable to be evaluated
@@ -7,13 +8,5 @@
 
 void print_rev(char *s)
 {
-	char newline = '\n';
-	int len = strlen(s);
-	int i;
-
-	for (i = len; i > 0; i++)
-	{
-		write(1, (s+i), 1);
-	}
-	write(1, &newline, 1);
+	printf("%s\n", strrev(s));
 }
