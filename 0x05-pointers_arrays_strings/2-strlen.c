1bb1acbc46cac595855a62127ff6ca3a50f343aa
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-24 22:36:12 +0100

    done

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
+/**
+ * _strlen - calculate the length of a string
+ * @s: string variable
+ * @return: 0
+ */
+
 int _strlen(char *s)
 {
 	int count = 0;
 
-	while (*s)
+	while (*s  != '\0')
 	{
 		count++;
 		s++;
