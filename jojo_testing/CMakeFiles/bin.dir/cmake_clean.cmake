004de1a28fc1a9272823075d9602e62334317b97
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-24 15:23:28 -0700

    done

---
+++ jojo_testing/CMakeFiles/bin.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bin.dir/_putchar.c.o"
+  "CMakeFiles/bin.dir/main.c.o"
+  "bin"
+  "bin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
