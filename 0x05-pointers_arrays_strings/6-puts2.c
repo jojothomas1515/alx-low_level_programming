76631cbd64ab0b6fb861d9d5e0704cbbf78bf864
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-26 00:32:08 +0100

    mimic

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - print every even index character
+ * @str: string to be process
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for( i = 0; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,6 +10,7 @@ void _puts(char *str);
 void print_rev(char *s);
 void _putchar(char c);
 void rev_string(char *s);
+void puts2(char *str);
 
 
 #endif /* JOJO_TESTING_MAIN_H */
