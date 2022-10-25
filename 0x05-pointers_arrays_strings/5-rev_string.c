fc90e869847ecf77ac74643b6276e6a7f8b215de
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-25 23:12:39 +0100

    mimic

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,16 +6,16 @@
 void rev_string(char *s)
 {
 	int l, i;
-	char ch;
+	char temp;
 
 	for (l = 0; s[l] != '\0'; ++l)
 		;
 
 	for (i = 0; i < l / 2; ++i)
 	{
-		ch = s[i];
+		temp = s[i];
 		s[i] = s[l - 1 - i];
-		s[l - 1 - i] = ch;
+		s[l - 1 - i] = temp;
 	}
 
 }
