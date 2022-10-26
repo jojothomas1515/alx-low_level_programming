350d7b7745770dbc956661d2dcc32ac7e86680e9
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-26 08:44:35 +0100

    why na

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,11 +15,11 @@ void print_array(int *a, int n)
 		{
 			if (!(i == (n - 1)))
 			{
-				printf("%ld, ", a[i]);
+				printf("%d, ", a[i]);
 			}
 			else
 			{
-				printf("%ld", a[i]);
+				printf("%d", a[i]);
 			}
 		}
 	}
