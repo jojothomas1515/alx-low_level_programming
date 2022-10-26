ac5e7b60ce03e82fea1b08a9062ac2a3f683ebf2
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-26 16:53:07 +0100

    mimic

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,17 +7,13 @@
 int _atoi(char *s)
 {
 	unsigned int sign = 1, i;
-	int num = 0;
+	unsigned int num = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == '-')
 		{
 			sign = -1;
-		}
-		else if (s[i] == ' ' || s[i] == '+')
-		{
-			sign = +1;
 		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
