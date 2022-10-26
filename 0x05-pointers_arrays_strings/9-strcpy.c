c24f18adf0835c623159c1f7cd81bd669fef205d
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-26 09:22:02 +0100

    mimic

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+/**
+ * _strcpy - copy two string
+ * @dest: destination of the copy
+ * @src:  source of the copy
+ * Return: a character array
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest)
+}
