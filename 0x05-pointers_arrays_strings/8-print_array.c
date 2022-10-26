b426f8ebf4ce8b672044876931f74b9ca780da50
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-26 08:36:45 +0100

    mimic

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,20 +1,25 @@
 #include "main.h"
+
 /**
  * print_array - print the n elements of an array integers
  * @a: the array
  * @n: number of elements to be printed
  */
-void print_array(int *a, int n)
-{
-	int  i;
+void print_array(int *a, int n) {
+	int i;
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
-
-		if (i != (n - 1))
+		if ((i % 2) == 0)
 		{
-			printf(", ");
+			if (!(i == (n - 1)))
+			{
+				printf("%d, ", a[i]);
+			}
+			else
+			{
+				printf("%d", a[i]);
+			}
 		}
 	}
 	_putchar('\n');
