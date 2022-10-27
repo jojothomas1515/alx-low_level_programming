dcf3758feabd263d01703412f0fb3ee7e4e4ff03
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-27 14:20:16 +0100

    mimic

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,14 @@
 #include "main.h"
-
+/**
+ * _strncat -  concat string  by an number of bytes
+ * @dest: target or destination
+ * @src: source
+ * @n: bytes
+ * Returns a pointer to destination
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
 
 	for(i = 0; src[i] != '\0' && i < n; i++)
 	{
