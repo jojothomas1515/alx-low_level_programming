0c58a4cd6a892410dc4a9b24272d74d724798060
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-28 00:17:17 +0100

    mimic

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+/**
+ * string_toupper - convert string to uppercase
+ * @a: target string
+ * Return: pointer to a
+ */
+char *string_toupper(char *a)
+{
+	int i;
+	char c;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		c = 'a';
+
+		while (c < 'z')
+		{
+			if (c == a[i])
+			{
+				c = c - 'a';
+				a[i] = 65 + c;
+			}
+			c++;
+		}
+	}
+	return (a);
+}
