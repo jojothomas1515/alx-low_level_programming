d95363868625c1a9acb20a73e3db1c67db02c043
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-27 13:20:07 +0100

    mimic

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,12 +8,13 @@
 char *_strcat(char *dest, char *src)
 {
 	int c, j;
+
 	for (c = 0; dest[c] != '\0'; c++)
 		;
 
 	for (j = 0; src[j] != '\0'; j++)
-		dest[c+j] = src[j];
-	dest[c+j] = '\0';
+		dest[c + j] = src[j];
+	dest[c + j] = '\0';
 
 	return (dest);
 }
