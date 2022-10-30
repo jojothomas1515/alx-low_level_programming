c6c38a23a0d9f80271bc86973d286e77a4f390de
Jojo <jojothomas1515@gmail.com>
2022-10-30 18:01:39 +0100

    natural number sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of my program
+ *
+ * Description: This program sums all the natural numbers that are multiples
+ * of 3 or 5 int 1024
+ * Return: 0
+*/
+
+int main(void)
+{
+    int i, target, sum;
+
+    sum = 0;
+    target = 1024;
+    for (i=0; i <= target; i++)
+    {
+        if ((i % 3) == 0 || (i % 5) == 0)
+        {
+            sum += i;
+        }
+    }
+
+    printf("%d\n", sum);
+
+}
