4573e887c6b2c1ad8fb23663f7fb8b388d70a36c
Jojo <jojothomas1515@gmail.com>
2022-10-31 00:06:12 +0100

    prime factor

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,42 +6,19 @@
 
 void print_number(int n)
 {
-    if (((n <= 9) && (n >= 0)) || ((n <= -9) && (n >= -1)))
-    {
-        if (n < 0)
-        {
-            _putchar('-');
-        }
-        _putchar(n + 48);
-    }
-    else if (((n <= 99) && (n >= 10)) || ((n <= -99) && (n >= -10)) )
-    {
-        if (n < 0)
-        {
-            _putchar('-');
-        }
-        _putchar((n / 10) + 48);
-        _putchar((n % 10) + 48);
-    }
-    else if (((n >= 100) && (n <= 999)) || ((n >= -100) && (n <= -999)))
-    {
-        if (n < 0)
-        {
-            _putchar('-');
-        }
-        _putchar(((n / 10) / 10)  + 48);
-        _putchar(((n / 10) % 10) + 48);
-        _putchar((n % 10) + 48);
-    }
-    else if (((n >= 1000) && (n <= 9999)) || ((n >= -1000) && (n <= -9999)))
-    {
-        if (n < 0)
-        {
-            _putchar('-');
-        }
-        _putchar((n / 1000)  + 48);
-        _putchar(((n / 100) % 10) + 48);
-        _putchar(((n / 10) % 10) + 48);
-        _putchar((n % 10) + 48);
-    }
+	unsigned int num = n;
+
+	/*first check if its negative*/
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	/*print the first few digits*/
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	/*print the last digit*/
+	_putchar((num % 10) + 48);
 }
