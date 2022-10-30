9c001427e9e9699557ac6cc53e8514610bfcaf61
Jojo <jojothomas1515@gmail.com>
2022-10-30 19:11:25 +0100

    fib nums

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for my function
+ * Description: This program prints fib number
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int i, nt, sum = 0;
+	long int f = 0, s = 1;
+
+	nt = f + s;
+
+	for (i = 0; i < 4000000; i++)
+	{
+		if ((nt % 2) == 0)
+		{
+			sum += nt;
+		}
+/*
+ * printf("%ld", nt);
+*		if (i != 49)
+*		{
+*			putchar(',');
+*			putchar(' ');
+*		}
+*/
+
+		f = s;
+		s = nt;
+		nt = s + f;
+	}
+	printf("\n");
+	return (0);
+}
