20bbbe8bc50a8b90563e95ca7eba9d2531325b00
Jojo <jojothomas1515@gmail.com>
2022-11-01 01:06:37 +0100

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,26 +9,21 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0, j, f = 0;
 
-	while (haystack[i] != '\0')
+	char *h, *n;
+
+	while (*haystack != '\0')
 	{
-		if (haystack[i] == needle[0])
+		h = haystack;
+		n = needle;
+		while (*n != '\0' && *haystack == *n)
 		{
-			f = 0;
-			for (j = 0; needle[j] != '\0'; j++)
-			{
-				if (haystack[i + j] != needle[j])
-				{
-					f = 1;
-					break;
-				}
-			}
-			if (f != 1)
-				return ((haystack + i));
-
+			haystack++;
+			n++;
 		}
-		i++;
+		if (!*n)
+			return (h);
+		haystack++;
 	}
-	return (NULL);
+	return ('\0');
 }
