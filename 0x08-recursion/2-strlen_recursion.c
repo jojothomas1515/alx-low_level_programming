e1757a89cb70341c9dd67205fcd6d5ee5176b34e
Jojo <jojothomas1515@gmail.com>
2022-11-02 23:03:04 +0100

    reverse

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen(s + 1));
+	}
+	return (0);
+}
+
