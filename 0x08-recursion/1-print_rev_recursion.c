5123aaed3db1f49cd72290f2a5e0e0713d3a5dbe
Jojo <jojothomas1515@gmail.com>
2022-11-02 22:58:42 +0100

    reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,25 +11,10 @@ int _strlen(char *s);
 
 void _print_rev_recursion(char *s)
 {
-	int length = _strlen(s);
 
-	if (length != 0)
-	{
-		_putchar(*(s + (length - 1)));
-		_print_rev_recursion(((s - 1 ) + (length -1)));
-
-	}
-	else
-		_putchar('\n');
-}
-
-int _strlen(char *s)
-{
 	if (*s != '\0')
 	{
-		return (1 + _strlen(s + 1));
+		_print_rev_recursion((s + 1));
+		_putchar(*s);
 	}
-	return (0);
 }
-
-
