387ff73d1c203875316c030cbabf012940fea0a6
Jojo <jojothomas1515@gmail.com>
2022-11-02 22:16:45 +0100

    reverse string with recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,17 @@
-#inlude "main.h"
+#include "main.h"
 
 /**
- * _print_rev)recursion
+ * _print_rev_recursion - recursion
+ * @s: string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_print_rev_recursion((s + 1));
+	}
+	else
+		_putchar('\n');
+}
