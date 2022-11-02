ca104ce53e818edc9f34a51c35bcc2eaec997c67
Jojo <jojothomas1515@gmail.com>
2022-11-02 21:55:01 +0100

    i hope this fixes it

--- jojo_testing/main.c
+++ jojo_testing/main.c
@@ -1,3 +1,21 @@
-//
-// Created by jojo on 10/31/22.
-//
+#include <stdio.h>
+
+int print(int nb)
+{
+	if (nb < 0)
+	{
+		return (0);
+	}
+	printf("%d\n", nb);
+	printf("%d", nb + print(nb - 1));
+	printf(" %d\n", nb);
+
+	nb --;
+	return (nb);
+}
+
+int main(void)
+{
+	print(4);
+	return (0);
+}
