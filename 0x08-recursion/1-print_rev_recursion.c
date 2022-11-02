d76de5bf8c42f8b52d1ee70cb41846759a54e2ab
Jojo <jojothomas1515@gmail.com>
2022-11-02 22:45:46 +0100

    reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,34 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _print_rev_recursion - recursion
  * @s: string
  */
 
+
+int _strlen(char *s);
+
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	int length = _strlen(s);
+
+	if (length != 0)
 	{
-		_putchar(*s);
-		_print_rev_recursion((s + 1));
+		_putchar(*s + length);
+		_print_rev_recursion((s - 1 + length));
 	}
 	else
 		_putchar('\n');
 }
+
+int _strlen(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen(s + 1));
+	}
+	return (0);
+}
+
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,8 @@
 #ifndef INC_0X07_POINTERS_ARRAYS_STRINGS_MAIN_H
 #define INC_0X07_POINTERS_ARRAYS_STRINGS_MAIN_H
-void _puts_recursion(char *s);
+#include <stdio.h>
 
+void _puts_recursion(char *s);
 int _putchar(char s);
 void _print_rev_recursion(char *s);
 
