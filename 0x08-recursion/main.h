1505d9139f2dbebfdcc42f087c88a3699f9ae01f
Jojo <jojothomas1515@gmail.com>
2022-11-04 05:51:22 +0100

    work please

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,10 +2,13 @@
 #define INC_0X07_POINTERS_ARRAYS_STRINGS_MAIN_H
 #include <stdio.h>
 
+
+int _putchar(char c);
 void _puts_recursion(char *s);
-int _putchar(char s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
 
 #endif
