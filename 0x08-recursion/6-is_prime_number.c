0b1e8220f21a76d19a0b19c4849daf0197f29dce
Jojo <jojothomas1515@gmail.com>
2022-11-04 06:01:16 +0100

    work please

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -16,10 +16,9 @@ int divisors(int n, int m)
 	} else if (m / 2 > n)
 	{
 		return (divisors(n + 2, m));
-	} else
-	{
-		return (1);
 	}
+	return (1);
+
 }
 
 /**
@@ -33,8 +32,8 @@ int is_prime_number(int n)
 	if ((!(n % 2) && n != 2) || n < 2)
 	{
 		return (0);
-	} else
-	{
-		return (divisors(3, n));
 	}
+
+	return (divisors(3, n));
+
 }
