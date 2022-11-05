22942fbe8634c5253830e4be5b1e52c6517fd4ee
Jojo <jojothomas1515@gmail.com>
2022-11-05 23:23:58 +0100

    2.args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program entry point
+ * @argc: command line argument count
+ * @argv: command line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if ((argc - 1) < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	result = atoi(argv[1]) * atoi(argv[2]);
+	return (0);
+}
+
