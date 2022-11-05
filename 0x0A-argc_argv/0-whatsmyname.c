23aadaa056d1909af72df21747568c7ab50ac869
Jojo <jojothomas1515@gmail.com>
2022-11-05 22:08:37 +0100

    command line argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * @argc: command line argument count
+ * @argv: command line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
