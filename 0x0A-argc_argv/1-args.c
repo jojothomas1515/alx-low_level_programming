17724997a7ede4256ba9dbaf194db55436574a01
Jojo <jojothomas1515@gmail.com>
2022-11-05 22:53:45 +0100

    1.args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * @argc: command line argument count
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
