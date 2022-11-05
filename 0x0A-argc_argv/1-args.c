64660b159eab31be62264c64db4a402637450f5f
Jojo <jojothomas1515@gmail.com>
2022-11-05 23:03:39 +0100

    1.args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#define UNUSED(X) void(x);
 
 /**
  * main - program entry point
  * @argc: command line argument count
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char (*argv[]))
 {
+	(void) argv;
 	printf("%d\n", argc);
 	return (0);
 }
