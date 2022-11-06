2932b929f812bbf5ecd10afbfb55479618eb1c79
Jojo <jojothomas1515@gmail.com>
2022-11-06 13:53:15 +0100

    4-add.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	result = (atoi(argv[1]) + atoi(argv[2]));
+	result = (atoi(argv[1]) * atoi(argv[2]));
 	printf("%d\n", result);
 	return (0);
 }
