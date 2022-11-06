06cf35203e1ef2859667c52ebf43f4cd5ac06413
Jojo <jojothomas1515@gmail.com>
2022-11-06 12:04:18 +0100

    forgot to print the  value for result

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,14 +11,13 @@ int main(int argc, char *argv[])
 {
 	int result;
 
-	if ((argc - 1) < 2)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
-	} else
-	{
-		result = atoi(argv[1]) * atoi(argv[2]);
-
 	}
+
+	result = (atoi(argv[1]) + atoi(argv[2]));
+	printf("%d\n", result);
 	return (0);
 }
