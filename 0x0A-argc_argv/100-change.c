048a689d471d1f00a7fff0ba16769b0d78853dfa
Jojo <jojothomas1515@gmail.com>
2022-11-06 14:14:27 +0100

    100-change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * main - program entry point
+ * @argc: command line argument count
+ * @argv: command line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int coin, cent;
+
+	if (argc != 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		cent = atoi(argv[1]);
+
+		while (cent > 0)
+		{
+			if (cent >= 25)
+				cent -= 25;
+			else if ( cent >= 10)
+				cent -= 10;
+			else if (cent >= 5)
+				cent -= 5;
+			else if (cent >= 2)
+				cent -= 2;
+			else if (cent >= 1)
+				cent -= 1;
+			coin += 1;
+		}
+		printf("%d\n", coin);
+	}
+	return (0);
+}
