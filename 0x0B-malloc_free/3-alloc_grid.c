2959ef8f699842e44fe5e5f7c88e598ab8766f63
Jojo <jojothomas1515@gmail.com>
2022-11-07 23:31:49 +0100

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,6 +24,14 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		ptr[i] = (int *) malloc(sizeof(int) * width );
+
+		if (ptr[i] == NULL)
+		{
+			for (i++; i >= 0; i--)
+				free(ptr[i]);
+			free(ptr);
+			return (NULL);
+		}
 	}
 
 	if (ptr == NULL)
