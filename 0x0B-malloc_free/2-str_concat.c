b95aa406b72eda7889e875ef050e33c2580c2143
Jojo <jojothomas1515@gmail.com>
2022-11-07 18:52:35 +0100

    He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenate two strings and return the mem addr
+ * of the new string
+ * @s1: destination
+ * @s2: source
+ * Return: new addr
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int s1_LEN, s2_LEN, i;
+	char *ptr;
+
+	i = 0;
+	s1_LEN = strlen(s1) - 1;
+	s2_LEN = strlen(s2) - 1;
+
+	ptr = malloc(s1_LEN + s2_LEN +1);
+
+	while (*s1 != '\0')
+	{
+		ptr[i] = *s1;
+		s1++;
+		i++;
+	}
+	while (*s2 != '\0')
+	{
+		ptr[i] = *s2;
+		s2++;
+		i++;
+	}
+	i++;
+	ptr[i] = '\0';
+	return (ptr);
+}
