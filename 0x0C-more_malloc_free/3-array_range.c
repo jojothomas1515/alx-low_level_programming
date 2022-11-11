aa30df379b7e9df29e42d826aa76029298858ce8
Jojo <jojothomas1515@gmail.com>
2022-11-11 21:42:34 +0100

    show me the error

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,8 +15,8 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	size = max - min;
-	ptr = malloc(size * sizeof(int) + 1);
+	size = max - min + 1;
+	ptr = malloc(size * sizeof(int));
 	if (ptr == NULL)
 		return (NULL);
 
