633bb4794d730816bc06e020cf7a9316e654e1b0
Jojo <jojothomas1515@gmail.com>
2022-11-11 17:17:16 +0100

    done

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * malloc_checked - if memory allocation failed it exit with a status code 98
+ * @b: number of bytes
+ * Return: memory address if successful , or exit with status code 98
+ */
+
 void *malloc_checked(unsigned int b)
 {
 	int *ptr;
