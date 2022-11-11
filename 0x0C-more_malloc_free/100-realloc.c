0dfdd2d7068e73eafceccc034bdcedcf0b3369fc
Jojo <jojothomas1515@gmail.com>
2022-11-11 22:30:29 +0100

    show me the error

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _realloc - reallocates memory
+ * @ptr: pointer to the memory address
+ * @old_size: the old size of memory
+ * @new_size: the size of memory
+ * Return: returns  a pointer
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (ptr == NULL && new_size != 0)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr != 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	ptr = malloc(new_size);
+	return (ptr);
+}
