27ea4a321c8cdcd1446e69bc93ab2b792a542d70
Jojo <jojothomas1515@gmail.com>
2022-11-11 21:02:13 +0100

    show me the error

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * array_range - make and array with element withing the range of min and max
+ * @min: minimum range value
+ * @max: maximum range value
+ * Return: the address to  the array
+ */
+
+int *array_range(int min, int max)
+{
+	unsigned int size, i;
+	int *ptr;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min;
+	ptr = malloc(size * sizeof(int));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; i++, min++)
+	{
+		ptr[i] = min;
+	}
+
+	return (ptr);
+}
