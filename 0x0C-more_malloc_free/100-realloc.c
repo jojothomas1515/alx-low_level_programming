bf52c27d8fe5ea505c7c4007e86ee86d7f83f23b
Jojo <jojothomas1515@gmail.com>
2022-11-12 02:06:28 +0100

    show me the error

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -36,9 +36,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i <= old_size; i++)
+	for (i = 0; i < old_size; i++)
 		new_ptr[i] = tmp_ptr[i];
-	new_ptr[i] = '\0';
 	free(ptr);
 	return (new_ptr);
 }
