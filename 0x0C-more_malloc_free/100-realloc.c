3941ec815a334d9c8459d3e3dc50e9b0e661e316
Jojo <jojothomas1515@gmail.com>
2022-11-12 02:00:00 +0100

    show me the error

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,10 +15,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL && new_size != 0)
 	{
-		ptr = malloc(new_size);
-		if (ptr == NULL)
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
 			return (NULL);
-		return (ptr);
+		free(ptr);
+		return (new_ptr);
 	}
 	if (new_size == old_size)
 		return (ptr);
