f234aa73c05a7cdc7af20b4b56d329f253d30d10
Jojo <jojothomas1515@gmail.com>
2022-11-17 10:47:06 +0100

    dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef JOJO_TESTING_DOG_H
 #define JOJO_TESTING_DOG_H
 
+/**
+ * struct dog - dog info
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
 struct dog{
     char *name;
     int *age;
