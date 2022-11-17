b0682e6083bccad136399a0845ca3372404e9d52
Jojo <jojothomas1515@gmail.com>
2022-11-17 19:03:00 +0100

    array_iterator function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - yes
+ * @array: target array
+ * @size: array size
+ * @action: function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (size == NULL || size < 0)
+		exit(1);
+	if (action == NULL || sizeof(array) < 1)
+		exit(1);
+	int i;
+
+	for (i = 0; i < size; i++)
+		(*action)(array[i]);
+}
