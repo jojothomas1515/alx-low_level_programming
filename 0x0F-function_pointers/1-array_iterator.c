43d77895e5560628330daf5aacdbb497fdabeecc
Jojo <jojothomas1515@gmail.com>
2022-11-17 22:08:14 +0100

    int_index function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,8 +13,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int index;
 
 	if (array != NULL && size > 0 && action != NULL)
+	{
 		for (index = 0; index < size; index++)
 			action(array[index]);
+	}
 	exit(1);
 }
 
