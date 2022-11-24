874a3adc8ccaf466f6fb0fb67e294676708d541c
Jojo <jojothomas1515@gmail.com>
2022-11-24 17:04:22 +0100

    add_node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_node - add node to the beginning of a linked list
+ * @head: pointer to there head node pointer
+ * @str: a string
+ */
+
+list_t *add_node(list_t **head, const char *str);
+{
+	list_t *old_node = *head;
+
+	list_t new_node, *new_node_ptr = &new_node;
+
+	new_node_ptr->str = strdup(str);
+	new_node_ptr->len = strlen(str);
+	new_node_ptr->next = old_node;
+
+	free(head);
+	return (new_node_ptr);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdio.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -21,5 +22,6 @@ typedef unsigned int _u_int;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif /* LIST_H */
