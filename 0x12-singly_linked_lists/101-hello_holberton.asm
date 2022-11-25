a4e5ac8e612e4162ea91d529bf2f376a69af89d0
Jojo <jojothomas1515@gmail.com>
2022-11-25 07:39:26 +0100

    i dont know what i did actually

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,22 @@
-;Declaring the needed c function
-        extern printf
+; Declare needed C  functions
+	extern	printf		; the C function, to be called
 
-        section .data
-msg:    db "Hello Holberton!", 0
-fmt:    db "%s",16, 0;
+	section .data		; Data section, initialized variables
+msg:	db "Hello, Holberton", 0; C string needs 0
+fmt:	db "%s", 10, 0		; The printf format, "\n",'0'
 
-        section .text
+	section .text		; Code section.
 
+	global main		; the standard gcc entry point
+main:				; the program label for the entry point
+	push	rbp		; set up stack frame, must be alligned
 
-        global main
-main:
-        push rbp;
-        mov rdi,fmt;
-        mov rsi,msg;
-        mov rax,0;
-        call printf;
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0		; or can be  xor  rax,rax
+	call	printf		; Call C function
 
-        pop rbp;
+	pop	rbp		; restore stack
 
-        mov rax,0;
-        ret;
+	mov	rax,0		; normal, no error, return value
+	ret			; return
