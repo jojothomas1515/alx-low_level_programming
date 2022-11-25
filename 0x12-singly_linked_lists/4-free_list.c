047fb0993ac4b64f29dde11f0940277e28166f36
Jojo <jojothomas1515@gmail.com>
2022-11-25 05:54:38 +0100

    free_list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,13 +8,21 @@
 
 void free_list(list_t *head)
 {
-	if (head == NULL)
-		exit(-1);
+	list_t *curr_node;
 
-	if (head->next != NULL)
-		free_list(head.next);
-	if (head->str != NULL)
-		free(head->str);
-	free(head);
-	exit(1);
+	/**
+	 * set curr_node to the head node 
+	 * then set the head node to the next node 
+	 * then free the memory containing the string
+	 * the free the memory containing the next
+	 * do ot over and over again till the curr is equal null
+	 */
+	curr_node = head;
+	while (curr_node != NULL)
+	{
+		head = head->next;
+		free(curr_node->str);
+		free(curr_node->next);
+		curr_node = head;
+	}
 }
