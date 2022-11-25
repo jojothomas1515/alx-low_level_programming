bd8da758dca820a813796659b83c0aa1c70f0605
Jojo <jojothomas1515@gmail.com>
2022-11-25 22:34:33 +0100

    i dont know what i did actually

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,18 +3,6 @@
 void __attribute__((constructor)) print_line(void);
 
 /**
- * main - entry point for my program
- * Return: 0
- */
-
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of\
-	a hare's nature, challenges one to a race.)\n");
-	return (0);
-}
-
-/**
  * print_line - prints a string
  */
 void print_line(void)
