9ec96fb462ce295c82643591e5de70bd982cc9fc
Jojo <jojothomas1515@gmail.com>
2022-11-25 06:49:12 +0100

    constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void print_line() __attribute__((constructor));
+
+/**
+ * main - entry point for my program
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
+}
+
+/**
+ * print_line - prints a string
+ */
+void print_line(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
