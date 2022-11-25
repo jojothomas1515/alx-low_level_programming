83a8f3f9949ace3f567e8bb6f8afc7216bd6a1d1
Jojo <jojothomas1515@gmail.com>
2022-11-25 07:30:49 +0100

    i dont know what i did actually

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,22 @@
+;Declaring the needed c function
+        extern printf
+
+        section .data
+msg:    db "Hello Holberton!", 0
+fmt:    db "%s";
+
+        section .text
+
+
+        global main
+main:
+        push rbp;
+        mov rdi,fmt;
+        mov rsi,msg;
+        mov rax,0;
+        call printf;
+
+        pop rbp;
+
+        mov rax,0;
+        ret;
