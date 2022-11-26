9e06c3491e51db154835b66457f658a154cd1966
Jojo <jojothomas1515@gmail.com>
2022-11-26 09:44:21 +0100

    rot13 encryption

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes str with rot encryption
+ * @str: target string to be encrypted
+ * Return: memory address of the string
+*/
+
+char *rot13(char *str)
+{
+	if (str == NULL)
+		return (NULL);
+	int i, j;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char encoder[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = encoder[j];
+			}
+		}
+	}
+	return (str);
+}
