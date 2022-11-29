e45f6765f38e3119c21d7096c65ad5006cfd6053
Jojo <jojothomas1515@gmail.com>
2022-11-29 15:36:14 +0100

    insert node at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * insert_nodeint_at_index - This function inserts a node at a given index
  * @head: pointer to the head node pointer
@@ -9,21 +8,30 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node;
+	listint_t *new_node, *curr_node;
+	_u_int i;
 
 	if (head != NULL && *head != NULL)
 	{
-
 		listint_t *index_node;
 		listint_t *pindex_node;
 
+		curr_node = *head;
 		new_node = malloc(sizeof(listint_t));
 		if (new_node == NULL)
 			return (NULL);
+
 		new_node->n = n;
 		new_node->next = NULL;
-		pindex_node = get_nodeint_at_index(*head, (idx - 1));
-		index_node = get_nodeint_at_index(*head, idx);
+
+		for (i = 0; curr_node && (i <= idx); i++)
+		{
+
+			pindex_node = curr_node;
+			curr_node = curr_node->next;
+			index_node = curr_node;
+		}
+
 		pindex_node->next = new_node;
 		new_node->next = index_node;
 		return (new_node);
