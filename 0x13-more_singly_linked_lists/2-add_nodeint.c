1ff3bb68fed41f3f20a17a5f898ab00bd3a26b58
Jojo <jojothomas1515@gmail.com>
2022-11-29 06:18:19 +0100

    made betty happy and fixed a typo

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,7 +4,7 @@
  * add_nodeint - and node to the beginning of the list
  * @head: pointer to the node pointer
  * @n: the value to be added to the list
- * Return the address of the new node
+ * Return: the address of the new node
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
@@ -12,7 +12,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *new_node_ptr;
 	listint_t *old_node_ptr = *head;
 
-	if (old_node_ptr == NULL)
+	if (*head == NULL)
 	{
 		old_node_ptr = malloc(sizeof(listint_t));
 		old_node_ptr->n = n;
@@ -25,5 +25,5 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new_node_ptr->n = n;
 	new_node_ptr->next = old_node_ptr;
 	*head = new_node_ptr;
-	return (*head);
+	return (new_node_ptr);
 }
