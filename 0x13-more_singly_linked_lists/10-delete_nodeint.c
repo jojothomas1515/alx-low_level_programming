e490a64755f287038719494654dae7fe9125e7df
Jojo <jojothomas1515@gmail.com>
2022-11-29 18:04:12 +0100

    insert node at a given index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,10 +16,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		curr_node = head_node_ptr;
 
-		while (curr_node && i++ < index)
+		while (curr_node && i < index)
 		{
 			prev_node_ptr = curr_node;
 			curr_node = curr_node->next;
+			i++
 		}
 		prev_node_ptr->next = curr_node->next;
 		free(curr_node);
