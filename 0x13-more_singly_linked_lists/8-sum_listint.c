fec84faf7a7bbe34766533fd1e95b20ee3b85e13
Jojo <jojothomas1515@gmail.com>
2022-11-29 12:31:15 +0100

    sum all the values

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,24 +2,24 @@
 
 /**
  * sum_listint - sums the int in the linked list
- * @head: pointer to the linked list 
+ * @head: pointer to the linked list
  * Return: the sum
-*/
+ */
 
 int sum_listint(listint_t *head)
 {
-        listint_t *curr_node;
-        int result = 0;
+	listint_t *curr_node;
+	int result = 0;
 
-        if (head == NULL)
-                return (0);
+	if (head == NULL)
+		return (0);
 
-        curr_node = head;
+	curr_node = head;
 
-        while (curr_node)
-        {
-                result += curr_node->n;
-                curr_node = curr_node->next;
-        }
-        return (result);
+	while (curr_node)
+	{
+		result += curr_node->n;
+		curr_node = curr_node->next;
+	}
+	return (result);
 }
