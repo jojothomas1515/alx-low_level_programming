4f40c0c30c2843b4d1d6d389977e8db9d0417af8
Jojo <jojothomas1515@gmail.com>
2022-11-29 08:03:17 +0100

    made a function to free memory allocated to linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - the the memory occupied by nodes in a linked list
+ * @head: the point the first/head node
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *curr_node, *temp;
+
+	if (head == NULL)
+		exit(EXIT_SUCCESS);
+	curr_node = head;
+
+	/* iterate through the linked list and free each node */
+	while (curr_node)
+	{
+		temp = curr_node;
+		curr_node = curr_node->next;
+		free(temp);
+	}
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -22,11 +22,11 @@ typedef struct listint_s
 typedef unsigned int _u_int;
 
 /* prototype declaration */
-
 int _putchar(char);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
 #endif /* LIST_H */
