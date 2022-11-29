e0c974d92c831e48df51eb33a24d35edf3a15f71
Jojo <jojothomas1515@gmail.com>
2022-11-29 08:46:26 +0100

    free list and set the head pointer to point to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free the linked list and set head to NULL
+ * @head: pointer to the pointer of n the first/head node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *curr_node, *temp;
+
+	/* check to see if head points to a node else do nothing */
+	if (*head != NULL)
+	{
+		curr_node = *head;
+
+		while (curr_node)
+		{
+			temp = curr_node;
+			curr_node = curr_node->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
