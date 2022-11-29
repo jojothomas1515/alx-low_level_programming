9a211acf6d4a679a01ed7c7f531db1240731d3c1
Jojo <jojothomas1515@gmail.com>
2022-11-29 05:52:00 +0100

    hmm

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,20 +8,20 @@
 
 size_t print_listint(const listint_t *h)
 {
-	_u_int counts = 0;
-	const listint_t *curr_node = h;
+	_u_int count = 1;
 
 	/* checking if the pointer is valid*/
 	if (h == NULL)
 		return (0);
 
-	/* check if node is valid and stop iterating if not*/
-	while (curr_node)
+	/* check if the struct integer n contains a value */
+	if (h->n)
 	{
-		if (curr_node->n)
-			counts += _putchar(curr_node->n + '0');
+		_putchar(h->n + '0');
 		_putchar('\n');
-		curr_node = curr_node->next;
 	}
-	return (counts);
+	else if (h->next == NULL)
+		return (count);
+	count += print_listint(h->next);
+	return (count);
 }
