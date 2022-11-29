f6ced580cb4d3897abe610bd9a011204deeeb461
Jojo <jojothomas1515@gmail.com>
2022-11-29 17:42:13 +0100

    insert node at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	else
 	{
-		new_node = add_nodeint_end(head, n);
+
 		*head = new_node;
 		return (new_node);
 	}
