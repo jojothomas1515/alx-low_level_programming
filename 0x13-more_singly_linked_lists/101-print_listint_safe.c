f2d6237a25835b67017a2c0faedd0ccbe1b9403c
Jojo <jojothomas1515@gmail.com>
2022-11-30 23:10:04 +0100

    done:

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,25 @@
+#include "list.h"
+
+/**
+ * print_listint_safe - print node list with just one loop
+ * and exit with code 98 if failed
+ * @head: pointer to the first node
+ * Return: the size if printed node
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	listint_t *curr_node;
+	int i = 0;
+
+	if (head == NULL || (*head) == NULL)
+		exit(98);
+
+	curr_node = (*head);
+	while (curr_node)
+	{
+		printf("%d\n", curr_node->n);
+		counts++;
+		curr_node = curr_node->next;
+	}
+	return (curr_node);
+}
