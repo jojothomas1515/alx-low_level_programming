ccdfc754e9876cdb0a6e3d43dc473648fa394864
Jojo <jojothomas1515@gmail.com>
2022-11-30 13:13:14 +0100

    some changes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,7 @@
 #include "lists.h"
+
+listint_t *make_head_node(listint_t *n_d, listint_t **head, int n);
+
 /**
  * insert_nodeint_at_index - This function inserts a node at a given index
  * @head: pointer to the head node pointer
@@ -8,13 +11,14 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *curr_node;
+	listint_t *new_node = NULL, *curr_node;
 	_u_int i;
 
 	if (head != NULL && *head != NULL)
 	{
 		listint_t *temp;
 		listint_t *pindex_node;
+
 		curr_node = *head;
 		new_node = malloc(sizeof(listint_t));
 		if (new_node == NULL)
@@ -42,11 +46,27 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	else
 	{
-		new_node = malloc(sizeof(listint_t));
-		if (new_node == NULL)
-			return (NULL);
-		new_node->n = n;
-		new_node->next = NULL;
-		return (*head = new_node);
+		return (make_head_node(new_node, head, n));
 	}
+}
+
+/**
+ * make_head_node - i have to make this function to bypass the
+ * 40 line per funtion rule
+ * @n_d: new_node
+ * @head: head node pointer
+ * @n: the value for the node to be created
+ * Return: a node address
+ */
+listint_t *make_head_node(listint_t *n_d, listint_t **head, int n)
+{
+	listint_t *new_node = n_d;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	*head = new_node;
+	return (new_node);
 }
