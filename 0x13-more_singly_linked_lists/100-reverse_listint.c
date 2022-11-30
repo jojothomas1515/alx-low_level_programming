7afeb2a7f215866688a9afec3e5f5d8a77dec935
Jojo <jojothomas1515@gmail.com>
2022-11-30 22:53:58 +0100

    done:

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -12,7 +12,7 @@ listint_t *reverse_listint(listint_t **head)
 
 	if ((*head) == NULL || head == NULL)
 		return (NULL);
-	if (!(*head)->next || (*head)->next == NULL)
+	if ((*head)->next == NULL)
 	{
 		(*head)->next = NULL;
 		return ((*head));
