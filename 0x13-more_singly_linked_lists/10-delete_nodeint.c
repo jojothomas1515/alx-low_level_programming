474c5277445b9769efabaab4512ecb19e63d8d2e
Jojo <jojothomas1515@gmail.com>
2022-11-30 08:51:33 +0100

    make some minor changes to the function to delete node at a certain index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,6 +10,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp, *curr_node = *head;
+	int i = 0;
 
 	(void)temp;
 	if (index == 0 && curr_node != NULL)
@@ -19,5 +20,17 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(curr_node);
 		return (1);
 	}
+	else if (index > 0 && curr_node)
+	{
+		while (curr_node && i < index)
+		{
+			temp = curr_node;
+			curr_node = curr_node->next;
+		}
+		temp->next = curr_node->next;
+		free(curr_node);
+		return (1);
+	}
+
 	return (-1);
 }
