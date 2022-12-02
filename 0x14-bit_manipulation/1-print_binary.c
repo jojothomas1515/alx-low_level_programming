5c528a38392da8832f87b06d9ca4c8b28f3c86e0
Jojo <jojothomas1515@gmail.com>
2022-12-02 18:25:06 +0100

    please work

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,89 +8,16 @@
 
 void print_binary(unsigned long int n)
 {
-	char *b_str = malloc(sizeof(char) * B_BUFF_SIZE);
 	_ul_int num = n, i = 0;
 
-	if (b_str == NULL)
-	{
-		_putchar('0');
-	}
 	if (n == 0)
 	{
 		_putchar('0');
 	}
 
-	while (!(num <= 0))
+	while (!((num >> i) <= 0))
 	{
-		b_str[i] = ((num % 2) + '0');
-		num = num / 2;
+		_putchar(((num >> i) & 1) + '0');
 		i++;
 	}
-	_strrev(b_str);
-	_print_string(b_str);
-}
-
-/**
- * _strlen - get the length of a string
- * @str: target string
- * Return: @str length
- */
-
-_u_int _strlen(const char *str)
-{
-	_u_int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	return (i);
-}
-
-/**
- * _strrev - reverse a string
- * @str: string to reverse
- * Return: the address of the reversed string
- */
-char *_strrev(char *str)
-{
-	_u_int len = _strlen(str), i = 0;
-	char c;
-
-	while (i != (len / 2))
-	{
-		c = str[i];
-		str[i] = str[(len - 1) - i];
-		str[(len - 1) - i] = c;
-		i++;
-	}
-	return (str);
-}
-
-/**
- * _print_string - this function prints out a string
- * @str: string passed as function argument
- */
-void _print_string(char *str)
-{
-	_u_int i = 0;
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-}
-
-/**
- * _strlen - get the length of a string
- * @str: target string
- * Return: @str length
- */
-
-_u_int _strlen(const char *str)
-{
-	_u_int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	return (i);
 }
