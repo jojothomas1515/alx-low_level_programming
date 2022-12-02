ba544804c573243fd5008d077332813ac9e6a43b
Jojo <jojothomas1515@gmail.com>
2022-12-02 18:37:56 +0100

    please work

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -14,17 +14,18 @@ void print_binary(unsigned long int n)
 	if (n == 0)
 	{
 		_putchar('0');
-		exit(0);
 	}
-
-	while (!((num >> i) <= 0))
+	else
 	{
-		i++;
-	}
-	i--;
-	while (i >= 0)
-	{
-		_putchar(((num >> i) & 1) + '0');
+		while (!((num >> i) <= 0))
+		{
+			i++;
+		}
 		i--;
+		while (i >= 0)
+		{
+			_putchar(((num >> i) & 1) + '0');
+			i--;
+		}
 	}
 }
