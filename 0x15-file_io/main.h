fd024119ec25a421fc689479be3b6a016d0696eb
Jojo Thomas <jojothomas1515@gmail.com>
2022-12-05 15:31:55 -0800

    read files

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Included header files */
+#include <fcntl.h>
+#include <unistd.h>
+
+/* definations */
+#define BUFF_SIZE 1024
+
+/* Prototype defination */
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* MAIN_H */
