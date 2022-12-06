8294f6c8d3f2baf5dd1c7758f804a4cd6c7f30ef
Jojo Thomas <jojothomas1515@gmail.com>
2022-12-05 16:57:58 -0800

    create files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -54,3 +54,29 @@ unsigned int _strlen(char *str)
 		;
 	return (count);
 }
+int create_file(const char *filename, char *text_content)
+{
+	int file, write_status, words = 0;
+
+	if (filename == NULL) /*check if filename is present*/
+		return (-1);
+
+	/*open file by creating it and if it exists write but truncate to 0*/
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1) /*check if file creation was a success*/
+		return (-1);
+
+	if (text_content) /*write content to file if its not NULL*/
+	{
+		while (text_content[words] != '\0') /*find number of words*/
+			words++;
+
+		/*write to file*/
+		write_status = write(file, text_content, words);
+		if (write_status == -1) /*check if write was a success*/
+			return (-1);
+	}
+
+	close(file); /*close file*/
+	return (1);
+}
