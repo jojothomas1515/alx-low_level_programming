b3ecd09d0930d6a0f87f4145a4d44e81a85e98c8
Jojo Thomas <jojothomas1515@gmail.com>
2022-12-05 16:22:32 -0800

    create file and write to file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+unsigned int _strlen(char *str);
+/**
+ * create_file - function that creates a file and write to it
+ * @filename: name of file to be created
+ * @text_content: to be added to the created file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | 0_WRONLY, 600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		if (write(fd, text_content, _strlen(text_content)) == -1)
+			return (-1);
+		return (1);
+	}
+	return (1);
+}
+
+/**
+ * _strlen - compute the length of a given string
+ * @str: target string
+ * Return: The length of the string
+ */
+
+unsigned int _strlen(char *str)
+{
+	unsigned int count = 0;
+
+	while (str[count++] != '\0')
+		;
+	count--;
+	return (count);
+}
