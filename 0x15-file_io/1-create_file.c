62b89105aa9db233ba6d79af96005da783365b0d
Jojo Thomas <jojothomas1515@gmail.com>
2022-12-05 17:01:10 -0800

    create files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,52 +10,6 @@ unsigned int _strlen(char *str);
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i = 0;
-	char *buff;
-
-	if (filename == NULL)
-		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
-	if (fd == -1)
-		return (-1);
-	if (text_content != NULL)
-	{
-		buff = malloc(sizeof(char) * _strlen(text_content));
-		if (buff == NULL)
-			return (-1);
-		while (text_content[i] != '\0')
-		{
-			buff[i] = text_content[i];
-			i++;
-		}
-		buff[i] = '\0';
-		if (write(fd, text_content, _strlen(text_content)) == -1)
-		{
-			free(buff);
-			return (-1);
-		}
-		free(buff);
-		return (1);
-	}
-	return (1);
-}
-
-/**
- * _strlen - compute the length of a given string
- * @str: target string
- * Return: The length of the string
- */
-
-unsigned int _strlen(char *str)
-{
-	unsigned int count = 0;
-
-	while (str[count++] != '\0')
-		;
-	return (count);
-}
-int create_file(const char *filename, char *text_content)
-{
 	int file, write_status, words = 0;
 
 	if (filename == NULL) /*check if filename is present*/
