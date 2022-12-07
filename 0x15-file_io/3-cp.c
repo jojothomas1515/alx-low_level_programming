9090cfc9037e4c9b779f90012f4ca1ad2bf68ddf
Jojo Thomas <jojothomas1515@gmail.com>
2022-12-07 03:34:06 -0800

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,80 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * @argc: Argument counts
+ * @argv: Arguments array
+ *
+ * Return: 0 on sucess
+ */
+
+int main(int argc, char *argv[])
+{
+	char *FILE_FROM, *FILE_TO, txt_buff[2] = "\0\0";
+	int fd_fm, fd_ft;
+	int read_status, write_status;
+
+	if (argc != 3)
+	{
+		print_string("Usage: cp file_from file_to\n", STDERR_FILENO);
+		exit(97);
+	}
+	FILE_FROM = argv[1];
+	FILE_TO = argv[2];
+
+	fd_fm = open(FILE_FROM, O_RDONLY);
+	fd_ft = open(FILE_TO, O_CREAT|O_WRONLY|O_TRUNC, 0664);
+
+	if (fd_fm == -1)
+	{
+		print_string("Error: Can't read from file ", STDERR_FILENO);
+		print_string(FILE_FROM, STDERR_FILENO);
+		print_string("\n", STDERR_FILENO);
+		exit(98);
+	}
+	if (fd_ft == -1)
+	{
+		print_string("Error: Can't write to ", STDERR_FILENO);
+		print_string(FILE_TO, STDERR_FILENO);
+		print_string("\n", STDERR_FILENO);
+		exit(99);
+	}
+
+	while (1)
+	{
+		read_status = read(fd_fm, txt_buff, 1);
+
+		if (read_status == 0)
+			break;
+		write_status = write(fd_ft, txt_buff, 1);
+		if (write_status == -1)
+			exit(99);
+
+	}
+
+	return (0);
+}
+
+/**
+ * print_string - print string out to stdout
+ *
+ * @str: string to print
+ * @iostream: what iostream to write to
+ *
+ * Return: word count on success or -1 on fail
+ */
+
+int print_string(char *str, int fd)
+{
+	int words = 0, word_status = 0;
+
+	while (str[words] != '\0')
+		words++;
+	word_status = write(fd, str, words);
+	if (word_status != -1)
+	{
+		return(word_status);
+	}
+	return (-1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /* definations */
 #define BUFF_SIZE 1024
@@ -14,5 +15,8 @@
 size_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+/* My prototypes */
+int print_string(char *, int);
 
 #endif /* MAIN_H */
