932cd5cc37786b09ef078167959193910878859b
Jojo <jojothomas1515@gmail.com>
2022-12-25 20:20:15 +0100

    implemented print_number without array, using recursion

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,49 +1,19 @@
 #include "main.h"
 
 /**
- * num_length - computes the length of a number
- * @n: number passed as argument
- * Return: length of a number
- */
-int num_length(int n)
-{
-	int num = n, len = 0;
-
-	while (num != 0)
-	{
-		len += 1;
-
-		num = num / 10;
-	}
-	return (len);
-}
-
-/**
  * print_number - print number to the standard output
  * @n: number to be printed
  */
 void print_number(int n)
 {
-	int num = n, i = 0;
-	int len = num_length(num);
-	char num_str[1024];
+	int num = n;
 
-	num_str[len] = '\0';
-	len--;
-	if (num == 0)
-		_putchar('0');
 	if (num < 0)
 	{
 		_putchar('-');
 		num *= -1;
 	}
-	while (num != 0)
-	{
-		num_str[len] = (num % 10) + 48;
-		num /= 10;
-		len--;
-	}
-
-	while (num_str[i] != '\0')
-		write(1, &num_str[i++], 1);
+	if ((num / 10) != 0)
+		print_number((num / 10));
+	_putchar((num % 10) + 48);
 }
