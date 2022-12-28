9dbc4ca10bc3a2100b624d29a23e30ed106a8f32
Jojo <jojothomas1515@gmail.com>
2022-12-28 23:44:54 +0100

    101-mul.c attempted

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * main - multiply two number
+ * @ac: args count
+ * @av: args variable
+ */
+void main(int ac, char *av[])
+{
+	int i;
+	unsigned long result = 0;
+
+	if (ac != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (i = 0; av[1][i] != '\0'; i++)
+	{
+		if (av[1][i] < 48 || av[1][i] > 57)
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	for (i = 0; av[2][i] != '\0'; i++)
+	{
+		if (av[2][i] < 48 || av[2][i] > 57)
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+
+	result = (atoi(av[1]) * atoi(av[2]));
+
+	printf("%lu\n", result);
+	exit(EXIT_SUCCESS);
+}
