19cefc6eedbb3fb03f40cf630951146b49b429f9
Jojo <jojothomas1515@gmail.com>
2023-01-02 00:47:45 +0100

    added code to close file descriptor

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 {
 	char *FILE_FROM, *FILE_TO, txt_buff[2] = "\0\0";
 	int fd_fm, fd_ft;
-	int read_status, write_status;
+	int read_status, write_status, close_status;
 
 	if (argc != 3)
 	{
@@ -37,15 +37,20 @@ int main(int argc, char *argv[])
 
 	while (1)
 	{
-		read_status = read(fd_fm, txt_buff, 1);
+		read_status = read(fd_fm, txt_buff, 1024);
 
 		if (read_status == 0)
 			break;
-		write_status = write(fd_ft, txt_buff, 1);
+		write_status = write(fd_ft, txt_buff, read_status);
 		if (write_status == -1)
 			e_exit(99, FILE_TO, fd_ft);
-
 	}
+	close_status = close(fd_fm);
+	if (close_status == -1)
+		e_exit(100, NULL, fd_fm);
+	close_status = close(fd_ft);
+	if (close_status == -1)
+		e_exit(100, NULL, fd_ft);
 
 	return (0);
 }
