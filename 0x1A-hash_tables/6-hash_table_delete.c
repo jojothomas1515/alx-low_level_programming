79ad1a2a9cad4fca16de62d1db283668a0df353c
Jojo <jojothomas1515@gmail.com>
2023-02-03 22:58:50 +0100

    print hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int i = 0;
+	hash_node_t *cn, *temp;
+
+	if (ht != NULL)
+	{
+		for (; i < ht->size; i++)
+		{
+			if (ht->array[i])
+			{
+				cn = ht->array[i];
+				ht->array[i] = NULL;
+				while (cn)
+				{
+					temp = cn->next;
+					free(cn);
+					cn = temp;
+				}
+			}
+		}
+		free(ht->array);
+		free(ht);
+		ht = NULL;
+	}
+}
