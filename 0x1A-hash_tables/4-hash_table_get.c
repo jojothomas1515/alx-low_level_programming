018f0e44685817e8db00e0c587fc14e44ed0fd8e
Jojo <jojothomas1515@gmail.com>
2023-02-03 21:44:07 +0100

    key index

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,8 +11,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *cn = NULL;
 	unsigned long idx = 0;
 
-	if (ht == NULL)
+	if (ht == NULL || key == NULL)
 		return (NULL);
+
+	if (strcmp("", key) == 0)
+		return (NULL);
+
 	idx = key_index((unsigned char *) key, ht->size);
 	cn = ht->array[idx];
 	while (cn)
