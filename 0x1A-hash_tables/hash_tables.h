53c534ef41304d81bf18509bf080eaf8c7803b1b
Jojo <jojothomas1515@gmail.com>
2023-02-03 18:40:34 +0100

    ht

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -42,5 +42,6 @@ typedef struct hash_table_s
 
 /* functions prototype declaration */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str)
 
 #endif /* INC_0X1A_HASH_TABLES__HASH_TABLES_H_ */
