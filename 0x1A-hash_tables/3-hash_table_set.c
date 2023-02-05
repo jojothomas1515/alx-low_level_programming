35560cfb575bb1dc6ec9420a8a58bd3af94e3758
Jojo <jojothomas1515@gmail.com>
2023-02-06 00:06:36 +0100

    removed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,7 +36,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			if (strcmp(cn->key, key) == 0)
 			{
-				free(cn->value);
 				cn->value = strdup(value);
 				return (1);
 			}
